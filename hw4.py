acff8aaa5bf471842e988c129929ff89d86866ca
TreybenF <47151401+TreybenF@users.noreply.github.com>
2019-02-13 21:55:29 -0500

    Add files via upload

---
+++ hw4.py
@@ -0,0 +1,4 @@
+#Treyben Funderburg
+#CS3560 HW4
+#this program multiplies all multiple of 3 and 5 up to 1000 and prints out their sum
+print sum([i for i in range(1000) if i % 3 == 0 or i % 5 == 0])
